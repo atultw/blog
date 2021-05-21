770aca293a73cc0da5a79a26fd4920a9747a60d4
Atulya <a>
2021-05-21 08:14:36 -0700

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const { DateTime } = require("luxon");
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPlugin(syntaxHighlight);
+    eleventyConfig.addPassthroughCopy("src/img");
+
+    eleventyConfig.addFilter("readableDate", dateObj => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat("dd LLL yyyy");
+    });
+
+    return {
+        dir: {
+            input: "src",
+            output: "pub"
+        }
+    }
+};
